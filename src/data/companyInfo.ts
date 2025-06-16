79365077e699a7cd84825c0410d4098d51428873
naveed0000 <sknaveed99026@gmail.com>
2025-06-17 00:07:57 +0530

    create animated number for company acheived number

--- src/data/companyInfo.ts
+++ src/data/companyInfo.ts
@@ -1,17 +1,22 @@
-const companyInfo = [
+interface companyInfoTypes {
+  id: number;
+  value: number;
+  desc: string;
+}
+const companyInfo: companyInfoTypes[] = [
   {
     id: 1,
-    value: "26,000,000",
+    value: 26_000_000,
     desc: "active buyers globally",
   },
   {
     id: 2,
-    value: "400,000",
+    value: 400_000,
     desc: "product inquiries daily",
   },
   {
     id: 3,
-    value: "200",
+    value: 200,
     desc: "countries and regions represented",
   },
 ];
